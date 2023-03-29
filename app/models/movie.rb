acb7cff8ddf4a99a6c8317e25611ebdd79332ead
Ronak Gupta <ronak.gupta@deeporion.com>
2023-03-29 16:14:44 +0530

    On Create Removed and enum added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,21 +2,18 @@ class Movie < ApplicationRecord
   #Validation
   validates :title, presence: true, length: { maximum: 30 }
 
-  validates :budget, presence: true, on: :create
-  validates :homepage, presence: true, on: :create
-  validates :overview, presence: true, length: { minimum: 6 }, on: :create
+  validates :budget, presence: true
+  validates :homepage, presence: true
+  validates :overview, presence: true, length: { minimum: 6 }
 
-  validates :popularity, presence: true, on: :create
-  validates :release_date, presence: true, on: :create
-  validates :revenue, presence: true, if: :movie_released?, on: :create
-  validates :runtime, presence: true, on: :create
-  validates :status,
-            on: :create,
-            presence: true,
-            inclusion: {
-              in: %w[released production announced]
-            }
-  validates :tagline, length: { minimum: 3 }, on: :create
+  validates :popularity, presence: true
+  validates :release_date, presence: true
+  validates :revenue, presence: true, if: :movie_released?
+  validates :runtime, presence: true
+
+  enum :status, %i[released production announced]
+
+  validates :tagline, length: { minimum: 3 }
 
   #Callbacks
 
